e772db5f553e701a53480ee3a56d102559cb0763
Durucz Béla <30567460+the-developer-guy@users.noreply.github.com>
2024-02-08 20:23:09 +0100

    Added command line resolution parameter.

--- main.py
+++ main.py
@@ -38,7 +38,10 @@ if len(sys.argv) > 1 and sys.argv[1] == "debug":
     monitor_width, monitor_height = 960, 540
 else:
     root.attributes('-fullscreen', True)
-    monitor_width, monitor_height = 1920, 1080
+    try:
+        monitor_width, monitor_height = int(sys.argv[1]), int(sys.argv[2])
+    except:
+        monitor_width, monitor_height = 1920, 1080
 
 root.geometry(f"{monitor_width}x{monitor_height}")
 root.rowconfigure(0, weight=1)
